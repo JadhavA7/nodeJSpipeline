332a6c4073bab0616274c1bbb722212ee2dfa68f
akash <a@b.com>
2025-03-18 11:28:59 +0530

    deployfinal

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('deploy nodejs');
+ res.send('deploy nodejs final');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
